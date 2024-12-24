cb23e92a6b1dc240988e44a9d1b1f1ba1c78b9c6
Phenny <mwaisakaphenny@gmail.com>
2024-12-24 21:35:25 +0300

    Initial commit

---
+++ src/main/java/com/billing/WifiBilling/WifiBillingApplication.java
@@ -0,0 +1,13 @@
+package com.billing.WifiBilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiBillingApplication.class, args);
+	}
+
+}
